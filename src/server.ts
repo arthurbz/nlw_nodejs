071c6433db6f960490b4ca941f1d571639f3078d
arthurbz <arthurbziero@gmail.com>
2021-04-19 22:38:10 -0300

    creating first routes

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json(
+        {
+            message: "Hello world!"
+        }
+    );
+});
+
+app.post("/", (request, response) => {
+    return response.json(
+        {
+            message: "Hello, this is a POST request!"
+        }
+    );
+});
+
+app.listen(5000, () => console.log("Server is running on port 5000"));
